bb8144f2d5a3f4bc611138f24a869c4bf26311ee
Reivo <reivo.meedla@voco.ee>
2026-09-12 15:18:12 +0300

    LAB 1.2 - Esmane lahendus

--- LAB1_2.py
+++ LAB1_2.py
@@ -29,21 +29,47 @@ class ProductFilter:
     def filter_by_size_and_color(products, size, color):
         return [product for product in products if product.size == size and product.color == color]
 
-
-products = [
-   Product("Apple", Color.GREEN, Size.SMALL),
-   Product("Tree", Color.GREEN, Size.LARGE),
-   Product("House", Color.BLUE, Size.LARGE)
-]
-
-pf = ProductFilter()
-
-print("Green products:")
-for p in pf.filter_by_color(products, Color.GREEN):
-   print(p.name)
-
-print("Large blue products:")
-for p in pf.filter_by_size_and_color(products, Size.LARGE, Color.BLUE):
-   print(p.name)
+class BetterFilter:
+    @staticmethod
+    def filter(products, specification):
+        return [
+            product
+            for product in products
+            if specification.is_satisfied_by(product)
+        ]
 
 
+class ColorSpecification:
+    def __init__(self, color):
+        self.color = color
+
+    def is_satisfied_by(self, product):
+        return product.color == self.color
+
+    def __and__(self, other):
+        return AndSpecification(self, other)
+
+
+class SizeSpecification:
+    def __init__(self, size):
+        self.size = size
+
+    def is_satisfied_by(self, product):
+        return product.size == self.size
+
+    def __and__(self, other):
+        return AndSpecification(self, other)
+
+
+class AndSpecification:
+    def __init__(self, *specifications):
+        self.specifications = specifications
+
+    def is_satisfied_by(self, product):
+        return all(
+            specification.is_satisfied_by(product)
+            for specification in self.specifications
+        )
+
+    def __and__(self, other):
+        return AndSpecification(*self.specifications, other)
